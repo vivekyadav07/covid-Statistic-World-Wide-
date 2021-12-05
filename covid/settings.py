9617b7325767bb6a628f3f0e96fd7dbd898d0415
vivekyadav07 <vivekkumaryadav97@gmail.com>
2021-12-05 17:43:06 +0530

    make it better

--- covid/settings.py
+++ covid/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covidstat1.herokuapp.com']
+ALLOWED_HOSTS = ['covidstat21.herokuapp.com']
 
 
 # Application definition
